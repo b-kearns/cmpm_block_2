513d155d19b2476d4781309a852e342d0ee1c0c3
Caetano Hyams <cjhyams@ucsc.edu>
2019-11-06 19:33:06 -0800

    Bison are prefabs and are spawned by a herd object bison have in their script to space themselves, but it causes an error so it is commented out on line 50.

---
+++ Big Bang Bison/Assets/Scripts/Bison/BisonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BisonController : MonoBehaviour
+{
+    
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject BISON;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate bison.
+        for (int i = -2; i <= 2; i++)
+        {
+            for (int j = -2; j <= 2; j++)
+            {
+                GameObject bison = Instantiate(BISON, new Vector3(i, 1, j), Quaternion.identity);
+                bison.transform.parent = gameObject.transform;
+            }
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
